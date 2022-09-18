9da551cf09641765dddaae8693c0e1ef0674e089
kretiene <kretiene@gmail.com>
2022-09-18 03:58:37 +0000

    extra addings to ameesba

--- ameesba.sh
+++ ameesba.sh
@@ -1,3 +1,3 @@
  this is the association of all babadjou students. We were part of it from the late 80s to early 90s in yaounde, at the time of serious crises in Cameroon this is the association of all babadjou students. We were part of it from the late 80s to early 90s in yaounde, at the time of serious crises in Cameroon
 
-I  am proud to have been part of this group then
+I  am proud to have been part of this group then, when looking back
